7575e4d08950f55197e9df4afc195982bc849f1e
DeeBest <simphiwedladla8@gmail.com>
2024-04-30 22:43:44 +0200

    added event listeners to the play trailer button and the close popup button

--- index.js
+++ index.js
@@ -2,3 +2,20 @@ const currentYear = new Date().getFullYear();
 
 const footerParagraph = document.getElementById('footer-paragraph');
 footerParagraph.textContent = `Created By \xA9Simphiwe ${currentYear}.`;
+
+const watchTrailerBtn = document.getElementById('watch-trailer-btn');
+const closePopupBtn = document.getElementById('close-popup');
+const trailerVideoContainer = document.getElementById(
+  'trailer-video-container'
+);
+const video = document.querySelector('video');
+
+watchTrailerBtn.addEventListener('click', () => {
+  trailerVideoContainer.classList.add('active');
+});
+
+closePopupBtn.addEventListener('click', () => {
+  trailerVideoContainer.classList.remove('active');
+  video.pause();
+  video.currentTime = 0;
+});
