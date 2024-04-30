86c621ed602f75056e6e324a9407c4edd839c5e2
DeeBest <simphiwedladla8@gmail.com>
2024-04-30 19:58:10 +0200

    created html structure and added icons and media resources

---
+++ index.js
@@ -0,0 +1,4 @@
+const currentYear = new Date().getFullYear();
+
+const footerParagraph = document.getElementById('footer-paragraph');
+footerParagraph.textContent = `Created By \xA9Simphiwe ${currentYear}.`;
